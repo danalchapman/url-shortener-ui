1802bfb1561bdf62a58c0c45ebb4031a2a67f72d
Dana Chapman <dana.leigh.dlc@gmail.com>
2022-11-15 11:44:38 -0600

    Finish iteration 4 and add keys to shortened URLs

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -41,10 +41,10 @@ describe('User Flows', () => {
   })
 
   it('Should reflect the user input in the form fields', () => {
-    cy.get('input[name="title')
+    cy.get('input[name="title"]')
       .type('Pretty picture')
       .should('have.value', 'Pretty picture')
-    cy.get('input[name="urlToShorten')
+    cy.get('input[name="urlToShorten"]')
       .type('https://images.unsplash.com/photo-1509023464722-18d996393ca8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80')
       .should('have.value', 'https://images.unsplash.com/photo-1509023464722-18d996393ca8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80')
   })
@@ -56,20 +56,34 @@ describe('Form flows', () => {
     cy.intercept('http://localhost:3001/api/v1/urls', {
       "urls": [
         {
-            "id": 3,
-            "long_url": "https://images.unsplash.com/photo-1531898418865-480b7090470f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80",
-            "short_url": "http://localhost:3001/useshorturl/3",
-            "title": "Cool photo"
+          "id": 3,
+          "long_url": "https://images.unsplash.com/photo-1531898418865-480b7090470f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80",
+          "short_url": "http://localhost:3001/useshorturl/3",
+          "title": "Cool photo"
         }
       ]    
     })
+    cy.intercept('POST', 'http://localhost:3001/api/v1/urls', {
+      statusCode: 201,
+      body: {
+        "id": 4,
+        "long_url": "https://images.unsplash.com/photo-1477511801984-4ad318ed9846?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bGF2ZW5kZXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=400&q=60",
+        "short_url": "http://localhost:3001/useshorturl/4",
+        "title": "Lavender photo"
+      }
+    })
     cy.visit('http://localhost:3000/')
-    cy.get('input[name="title')
-      .type('Pretty picture')
-      .should('have.value', 'Pretty picture')
-    cy.get('input[name="urlToShorten')
-      .type('https://images.unsplash.com/photo-1509023464722-18d996393ca8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80')
-      .should('have.value', 'https://images.unsplash.com/photo-1509023464722-18d996393ca8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80')
-      
+    cy.get('input[name="title"]')
+      .type('Lavender photo')
+      .should('have.value', 'Lavender photo')
+    cy.get('input[name="urlToShorten"]')
+      .type('https://images.unsplash.com/photo-1477511801984-4ad318ed9846?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bGF2ZW5kZXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=400&q=60')
+      .should('have.value', 'https://images.unsplash.com/photo-1477511801984-4ad318ed9846?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bGF2ZW5kZXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=400&q=60')
+    cy.get('button').should('exist').contains('Shorten Please!').click()
+    cy.get('h3').contains('Lavender photo')
+    cy.get('a')
+      .contains('http://localhost:3001/useshorturl/4')
+    cy.get('p')
+      .contains('https://images.unsplash.com/photo-1477511801984-4ad318ed9846?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bGF2ZW5kZXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=400&q=60')
   })
 })
